1ca0ddf8fe0f43f9e8d55e94e492ec062c6edca8
Duleesha Gamalathge <duleeshagamalathge@gmail.com>
2026-01-16 21:53:12 +0530

    add DashboardComponent

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { UsersListComponent } from './components/users-list/users-list.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'users', pathMatch: 'full' },
-    { path: 'users', component: UsersListComponent }
+    { path: 'users', component: UsersListComponent },
+    { path: 'dashboard', component: DashboardComponent}
 ];

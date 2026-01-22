7eca9a1d901089b1224348ce9d2b5cc7f0c81e41
Duleesha Gamalathge <duleeshagamalathge@gmail.com>
2026-01-22 22:16:19 +0530

    add navbar

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { UsersListComponent } from './components/users-list/users-list.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, UsersListComponent],
+  imports: [RouterOutlet, CommonModule, UsersListComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

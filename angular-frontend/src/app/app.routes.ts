6d19d4c4cca74ca1c973f34ec11b77247fbbb3ea
Duleesha Gamalathge <duleeshagamalathge@gmail.com>
2026-01-24 15:38:31 +0530

    redirect set into dashboard

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { UsersListComponent } from './components/users-list/users-list.component
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'users', pathMatch: 'full' },
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     { path: 'users', component: UsersListComponent },
     { path: 'dashboard', component: DashboardComponent}
 ];

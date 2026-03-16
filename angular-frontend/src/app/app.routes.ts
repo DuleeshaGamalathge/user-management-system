28a366ab79d64536ca3cc3a99c78ccff1ff2dd51
Duleesha Gamalathge <duleeshagamalathge@gmail.com>
2026-03-16 21:24:39 +0530

    add profile page

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { UsersListComponent } from './components/users-list/users-list.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'users', component: UsersListComponent, canActivate: [authGuard] },
     { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard]},
-    { path: 'login', component:LoginComponent}
+    { path: 'login', component:LoginComponent},
+    { path: 'profile', component:ProfileComponent, canActivate: [authGuard]}
 ];

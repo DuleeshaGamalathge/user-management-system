91ff38f8f0266088a7f0aaeba9729b8b21904949
Duleesha Gamalathge <duleeshagamalathge@gmail.com>
2026-03-17 21:47:44 +0530

    implement auto logout on 401 using HTTP interceptor

--- angular-frontend/src/app/interceptors/auth.interceptor.ts
+++ angular-frontend/src/app/interceptors/auth.interceptor.ts
@@ -1,10 +1,14 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const token = authService.getToken();
+  const router = inject(Router);
 
   if (token) {
     req = req.clone({
@@ -14,5 +18,16 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     });
   }
 
-  return next(req);
+  return next(req).pipe(
+    catchError((error) => {
+
+      if (error.status === 401) {
+        // Token expired or unauthorized
+        authService.logout();
+        router.navigate(['/login']);
+      }
+
+      return throwError(() => error);
+    })
+  );
 };
